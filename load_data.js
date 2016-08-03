a86a637df9d045ca487a671f5ecbd7a9fcec0acd
Parthiv Bhagat <parthiv.bhagat@cerner.com>
2016-08-03 11:17:02 -0500

    Cleaned up code

--- load_data.js
+++ load_data.js
@@ -9,13 +9,11 @@
       $.when(pt).done(function(patient){
 		 var gender = patient.gender;
 		 var dob = new Date(patient.birthDate);
-		 var age = calculateAge(dob);
 		 var fname = patient.name[0].given.join(" ");
 		 var lname = patient.name[0].family.join(" ");
 		 
 		  p = defaultPatient();
 		  p.birthday = {value:dob};
-		  p.age = {value:age};
 		  p.gender={value:gender};
 		  p.givenName={value:fname};
 		  p.familyName={value:lname};
@@ -26,11 +24,6 @@
     return ret.promise();
   };
   
-  function calculateAge(birthday) { // birthday is a date
-    var ageDifMs = Date.now() - birthday.getTime();
-    var ageDate = new Date(ageDifMs); // miliseconds from epoch
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
- }
  
   function defaultPatient(){
 	return {
@@ -38,7 +31,6 @@
 	  ,'familyName':  {'value': null}
 	  ,'gender':      {'value': null}
 	  ,'birthday':    {'value': null}
-	  ,'age':         {'value': null}
     }
   };
 

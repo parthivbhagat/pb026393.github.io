ecf0c2d6052c9e8d1f7e53afbd0b4648a3f5cb13
Parthiv Bhagat <parthiv.bhagat@cerner.com>
2016-08-05 16:50:29 -0500

    Code review comments

---
+++ draw_visualization.js
@@ -0,0 +1,12 @@
+var draw_visualization = function() {
+    var table = '<table><thead><th>First Name</th><th>Last Name</th><th>Gender</th><th>Birth Date</th><th>Age</th></thead><tbody>';
+    table += '<tr>';            
+    table += '<td>' + p.givenName.value + '</td>';
+    table += '<td>' + p.familyName.value + '</td>';
+    table += '<td>' + p.gender.value + '</td>';
+    table += '<td>' + p.birthday.value + '</td>';
+    table += '<td>' + p.age.value + '</td>';
+    table += '</tr>';
+    table += '</tbody></table>';
+    $('#holder').html(table);
+};

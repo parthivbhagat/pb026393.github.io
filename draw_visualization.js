6a5f4ea77a2e64d6a525cc6ba22398a8a91fad3b
Parthiv Bhagat <parthiv.bhagat@cerner.com>
2016-08-09 09:54:03 -0500

    Made some AirBNB JS Guide changes

--- draw_visualization.js
+++ draw_visualization.js
@@ -1,5 +1,5 @@
-var draw_visualization = function(p) { 
-    var table = '<table><thead><th>First Name</th><th>Last Name</th><th>Gender</th><th>Birth Date</th><th>Age</th></thead><tbody>';
+function draw_visualization(p) { 
+    let table = '<table><thead><th>First Name</th><th>Last Name</th><th>Gender</th><th>Birth Date</th><th>Age</th></thead><tbody>';
     table += '<tr>';            
     table += '<td>' + p.fname.value + '</td>';
     table += '<td>' + p.lname.value + '</td>';

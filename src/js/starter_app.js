02dffdd2906d11653d3bf9ed0687e7fc3e19fbf8
Parthiv Bhagat <parthiv.bhagat@cerner.com>
2016-12-08 20:30:11 -0600

    troubleshoot

--- src/js/starter_app.js
+++ src/js/starter_app.js
@@ -22,24 +22,31 @@
     function get(url) {
 	  // Return a new promise.
 	  	return new Promise(function(resolve, reject) {
+	    // Do the usual XHR stuff
+	    var req = new XMLHttpRequest();
+	    req.open('GET', url);
 
-		  	var req = $.ajax({
-						type: "GET",
-						url: url,
-						data: "", //ur data to be sent to server
-						contentType: "application/json",  
-						dataType: "json",			
-						
-						success: function (data) {
-						  resolve(JSON.parse(req.response));
-							
-						},
-						error: function (x, y, z) {
-						   reject(Error(req.statusText));
-						}
-					});
-		    
-		    req.send();
+	    req.onload = function() {
+	      // This is called even on 404 etc
+	      // so check the status
+	      if (req.status == 200) {
+	        // Resolve the promise with the response text
+	        resolve(JSON.parse(req.response));
+	      }
+	      else {
+	        // Otherwise reject with the status text
+	        // which will hopefully be a meaningful error
+	        reject(Error(req.statusText));
+	      }
+	    };
+
+	    // Handle network errors
+	    req.onerror = function() {
+	      reject(Error("Network Error"));
+	    };
+
+	    // Make the request
+	    req.send();
 	  });
 	}
 
